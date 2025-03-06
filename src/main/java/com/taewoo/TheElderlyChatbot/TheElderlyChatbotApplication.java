7c297df4e93bf7e2e1cdd55c8d9ed46abe757877
taewoo <yun68000@naver.comgit config --global user.emaigit config --global user.email yun68000@naver.com>
2025-03-07 02:03:47 +0900

    first comit

---
+++ src/main/java/com/taewoo/TheElderlyChatbot/TheElderlyChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.taewoo.TheElderlyChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheElderlyChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheElderlyChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taewoo/TheElderlyChatbot/TheElderlyChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taewoo.TheElderlyChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheElderlyChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
